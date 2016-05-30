8fc6a1fe3c875e59d66c620a533038109edb3389
Igor Tsygin <Hakerok1995@gmail.com>
2016-05-30 19:13:51 +0700

    new

--- Expert_systems/Expert_systems/Classes/ClassDataGrid.cs
+++ Expert_systems/Expert_systems/Classes/ClassDataGrid.cs
@@ -7,7 +7,6 @@ namespace Expert_systems
 {
     public class ClassDataGrid 
     {
-      public List<ClassExpertModel> EMList = new List<ClassExpertModel>();
       public DataGridView dgKonstanty = new DataGridView();
       public DataGridView dgVariables = new DataGridView();
       
@@ -61,58 +60,6 @@ namespace Expert_systems
             dgVariables.AllowUserToDeleteRows = true;
             dgVariables.Columns.Add(Column_1);
             dgVariables.Columns.Add(Column_2);
-        }
-
-        public void Read_Hypothesis(string path)
-        {
-            string[] ruleStrings = File.ReadAllLines(path);
-            for (int i = 0; i < ruleStrings.Length; i++)
-            {
-                string[] rS = new string[5];
-                rS = ruleStrings[i].Split(',');
-                ClassExpertModel temp = new ClassExpertModel();
-                temp.Name = rS[0];
-                temp.Rule = rS[1];
-                temp.Parent = rS[2];
-                temp.Weight = Convert.ToDouble(rS[3].Replace('.', ','));
-                if (rS[4] == "false")
-                {
-                    temp.Obratimost = false;
-                }
-                else
-                {
-                    temp.Obratimost = true;
-                }
-                EMList.Add(temp);
-            }
-
-            for (int i = 0; i < EMList.Count; i++)
-            {
-                dgKonstanty.Rows.Add();
-                dgKonstanty.Rows[i].Cells[0].Value = EMList[i].Name;
-                dgKonstanty.Rows[i].Cells[1].Value = EMList[i].Rule;
-                dgKonstanty.Rows[i].Cells[2].Value = EMList[i].Parent;
-                dgKonstanty.Rows[i].Cells[3].Value = EMList[i].Weight;
-                dgKonstanty.Rows[i].Cells[4].Value = EMList[i].Obratimost;
-            }
-            for (int i = 0; i < EMList.Count; i++)
-            {
-                for (int j = 0; j < EMList.Count; j++)
-                {
-                    if (EMList[i].Name == EMList[j].Parent)
-                    {
-                        EMList[i].children.Add(EMList[j]);
-                    }
-                }
-            }
-            foreach (var z in EMList)
-            {
-                if (z.children.Count == 0)
-                {
-                    int rowNumber = dgVariables.Rows.Add();
-                    dgVariables.Rows[rowNumber].Cells[0].Value = z.Rule;
-                }
-            }
         }
 
     }
